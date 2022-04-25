026646ed823c9110b3e2e6689785c158a8cf240e
ajjeon <jeonahjung@gmail.com>
2022-04-25 14:01:39 +0800

    genome links fixed, mirdeep error fixed

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@
 nextflow run main.nf \
 	 -profile docker,aws \
 	 -c myconfig.config \
-	 -bucket-dir s3://ahjung/wgbs-test \
-	 --input 's3://directory/to/inputdata/prefix_R{1,2}.fq.gz' \
+	 -bucket-dir s3://ahjung/PNK/smrnaseq/ \
+	 --input 's3://00-sequencing-data-bgi/F22FTSAPHT0261_HUMamfrS/soapnuke/clean/*/*{1,2}.fq.gz' \
+	 --outdir 's3://ahjung/PNK/smrnaseq/results/' \
+	 -work-dir 's3://ahjung/PNK/smrnaseq/work/' \
 	 --genome GRCh38 \
 	 -resume
